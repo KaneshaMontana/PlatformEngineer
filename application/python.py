89726509e20b55b201ed25185f01007b262ecde7
Kanesha <93129317+KaneshaMontana@users.noreply.github.com>
2024-05-21 03:57:26 +0100

    Create python.py

---
+++ application/python.py
@@ -0,0 +1,7 @@
+name = input(string("Enter in your name: "))
+role = input(string("Enter in your role: "))
+
+def function (a,b){
+  a = name
+  b = role
+  return(f"My name is {a} and work as a {b})
